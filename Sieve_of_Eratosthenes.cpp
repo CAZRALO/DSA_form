f6a76e5ad9f298736b3722470ad4e90c14c190fd
CAZRALO <159870280+CAZRALO@users.noreply.github.com>
2024-06-21 07:37:31 +0700

    Update Sieve_of_Eratosthenes.cpp

--- Sieve_of_Eratosthenes.cpp
+++ Sieve_of_Eratosthenes.cpp
@@ -4,11 +4,14 @@ bool isPrime[1000005];
 void Sieve_of_Eratosthenes(int k) {
     memset(isPrime,true,sizeof(isPrime));
     isPrime[0]=false; isPrime[1]=false;
+    for (int i=2;i*i<=k;++i)
+        if (isPrime[i]) {
+            for (int j=i*i;j<=k;j+=i)
+                isPrime[j]=false;
+        }
     for (int i=2;i<=k;++i)
         if (isPrime[i]) {
             ++N;
             Prime[N]=i;
-            for (int j=i*i;j<=k;j+=i)
-                isPrime[j]=false;
         }
 }
