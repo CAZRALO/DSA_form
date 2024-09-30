25833aef5a4b0cdc50bf9c89a7bd4f3ad55ee605
CAZRALO <159870280+CAZRALO@users.noreply.github.com>
2024-09-30 10:51:08 +0700

    Update MaxtrixOperator.cpp

--- MaxtrixOperator.cpp
+++ MaxtrixOperator.cpp
@@ -1,5 +1,8 @@
+#define int long long
+const int MOD=1e9+7;
+
 class matrix {
-  int n,m;
+  int row,col;
   vector<vector<int>>arr;
 public:
   matrix(int n_, int m_) ;
@@ -17,16 +20,16 @@ public:
 };
 
 matrix :: matrix(int n_,int m) {
-    n=n_; m=m_;
+    row=n_; col=m_;
 }
 matrix :: matrix(vector<vector<int>> &a {
     arr=a;
-    n=a.size();
-    m=a[0].size();
+    row=a.size();
+    col=a[0].size();
 }
 
 matrix &operator = (const matrix &a) {
-    n=a.n; m=a.m; 
+    row=a.n; col=a.m; 
     arr=a.arr;
     return *this;
 }
@@ -47,13 +50,13 @@ matrix operator -(const matrix &a, const maxtrix &b) {
     return temp;
 }
 matrix &operator *=(matrix &a, const matrix &b) {
-  
-    matrix res(n,x.m);
+    matrix res(a.row,b.col);
     for (int u = 0; u < a.n; u++)
         for (int v = 0; v < b.m; v++)
             for (int i = 0; i < a.m; i++)
-                res.val[u][v] = ((ll)val[u][i] * x.val[i][v] + res.val[u][v] ) % MOD;
-  return res;
+                res.arr[u][v] = ((int)arr[u][i] * b.arr[i][v] + res.arr[u][v] ) % MOD;
+    a=res;
+    return a;
 }
 matrix operator  * (const matrix &a, const matrix &b) {
     matrix temp;
@@ -62,14 +65,20 @@ matrix operator  * (const matrix &a, const matrix &b) {
     return temp;
 }
 istream &operator>>(istream &in, matrix &a) {
-    in>>a.n>>a.m;
-    for (int i=0;i<a.n;++i) {
+    in>>a.row>>a.col;
+    for (int i=0;i<a.row;++i) {
+        for (int i=0;i<a.col;++i {
+            cin>>a.arr[i][j];
+        }
     }
-
     return i;
 }
 ostream &operator<<(ostream & , const matrix &a) {
-    for (int i=0;i<a.n;++i) {
+    for (int i=0;i<a.row;++i) {
+        for (int i=0;i<a.col;++i) {
+            cout<<a.arr[i][j]<<' ';
+        }
+        cout<<'\n';
     }
     return o;
 }
