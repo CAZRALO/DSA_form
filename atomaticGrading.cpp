8798fcd84e5e29d8c92cb813976d41a55aed5bec
CAZRALO <159870280+CAZRALO@users.noreply.github.com>
2024-10-25 13:42:15 +0700

    Create atomaticGrading.cpp

---
+++ atomaticGrading.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+#define FNAME ""
+
+int rd(int l,int r) {
+    return rand()*1ll*rand()%(r-l+1);
+} 
+
+void createTestcase() {
+    ofstream inp(FName".INP");
+    //inp>>;
+/*
+    sample : 
+    int n,m;
+    n=rd(1,1000); m=d(1,1000);
+    inp<<n<<m;
+*/
+    inp.close();
+}
+
+signed main() {
+    srand(NULL);
+    for (int iTest = 1; iTest <= 100; ++iTest) {
+        createTestcase();
+        system(ans".exe");
+        system(FName".exe");
+        if ("fc "+ans".OUT " + FName".OUT") {
+            cout<<iTest<<" WA";
+            return 0;
+        } else {
+            cout<<iTest<<" AC\n";
+        }
+    }
+}
